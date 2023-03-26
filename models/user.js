a90f45337e1b08f7184439eeb17d9e9b89a6b9d0
Vitalis Kirui <vitaliskirui1@gmail.com>
2023-03-26 19:54:59 +0300

    creating user model

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  names: {
+    type: String,
+    require: true,
+  },
+  username: {
+    type: String,
+    require: true,
+  },
+  phonenumber:{
+    type: Number,
+    require: true,
+  },
+  email:{
+    type: String,
+    require: true,
+  },
+  password:{
+    type: String,
+    require: true,
+  },
+  confirmPassword : {
+    type: String,
+    require: true,
+  },
+  subscribe:{
+    type: String
+  }
+},
+{timestamps:true}
+);
+
+module.exports = mongoose.model ('User' ,userSchema, 'users');
